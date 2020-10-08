2d89a16e7f461758dcf3a352f84cf830a4ec5a2b
daid <daid303@gmail.com>
2020-10-08 15:24:03 +0200

    Prevent super long data comment lines

--- badboyplugin.py
+++ badboyplugin.py
@@ -599,6 +599,6 @@ class ScriptBlock(Block):
 
         if msg is not None:
             file.asmLine(1, opcode[0], *args)
-            file.asmLine(size - 1, "  db", "\"%s\", $00" % (msg))
+            file.asmLine(size - 1, "  db", "\"%s\", $00" % (msg), is_data=True)
         else:
             file.asmLine(size, opcode[0], *args)
