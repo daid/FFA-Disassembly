8e524c9a6cc92dc463305ba36e0eca51edf451b5
Daid <daid303@gmail.com>
2022-04-03 10:26:59 +0200

    Add pointers to player attack graphics

--- plugins/attack.py
+++ plugins/attack.py
@@ -19,22 +19,21 @@ class AttackInfo(Block):
     def __init__(self, memory, addr):
         super().__init__(memory, addr, size=42)
         
-        #gfx_ptr = memory.word(addr + 6)
-        #self.gfx_bank = (gfx_ptr >> 14) + 0x08
-        #self.gfx_ptr = (gfx_ptr & 0x3FFF) | 0x4000
-        #RomInfo.romBank(self.gfx_bank).addAutoLabel(self.gfx_ptr, None, "data")
+        gfx_ptr = memory.word(addr + 6)
+        RomInfo.romBank(0x08).addAutoLabel(gfx_ptr, None, "data")
 
         for n in range(17):
             memory.addAutoLabel(memory.word(addr + 8 + n * 2), None, "data")
 
     def export(self, file):
-        file.asmLine(8, "db",
+        # 0) animation delay
+        # 6) pointer to graphics
+        file.asmLine(6, "db",
             "$%02x" % self.memory.byte(file.addr+0), "$%02x" % self.memory.byte(file.addr+1),
             "$%02x" % self.memory.byte(file.addr+2), "$%02x" % self.memory.byte(file.addr+3),
-            "$%02x" % self.memory.byte(file.addr+4), "$%02x" % self.memory.byte(file.addr+5),
-            "$%02x" % self.memory.byte(file.addr+6), "$%02x" % self.memory.byte(file.addr+7))
-        file.asmLine(2, "dw", str(self.memory.getLabel(self.memory.word(file.addr+0))))
+            "$%02x" % self.memory.byte(file.addr+4), "$%02x" % self.memory.byte(file.addr+5), is_data=True)
+        file.asmLine(4, "dw", str(RomInfo.romBank(0x08).getLabel(self.memory.word(file.addr))), str(self.memory.getLabel(self.memory.word(file.addr+2))), is_data=True)
         for n in range(4):
             file.asmLine(8, "dw",
                 str(self.memory.getLabel(self.memory.word(file.addr+0))), str(self.memory.getLabel(self.memory.word(file.addr+2))),
-                str(self.memory.getLabel(self.memory.word(file.addr+4))), str(self.memory.getLabel(self.memory.word(file.addr+6))))
+                str(self.memory.getLabel(self.memory.word(file.addr+4))), str(self.memory.getLabel(self.memory.word(file.addr+6))), is_data=True)
--- src/bank08.asm
+++ src/bank08.asm
@@ -26,8 +26,25 @@ INCLUDE "data/scriptPointers.asm"
     db   $0e, $6c, $b4, $4c, $0d, $2c, $b0, $4d        ;; 08:59e9 ????????
     db   $31, $0e, $6c, $b1, $4c, $0d, $2c, $ad        ;; 08:59f1 ????????
     db   $4d, $2e, $0e, $6c, $ae, $4c, $0d             ;; 08:59f9 ???????
-;@gfximg name=player width=2 height=40
-    INCBIN "player.bin"                                ;; 08:5a00
+    dw   `00000000                                     ;; 08:5a00 $00 $00
+    dw   `00000000                                     ;; 08:5a02 $00 $00
+    dw   `00000000                                     ;; 08:5a04 $00 $00
+    dw   `00000000                                     ;; 08:5a06 $00 $00
+    dw   `00000000                                     ;; 08:5a08 $00 $00
+    dw   `00000000                                     ;; 08:5a0a $00 $00
+    dw   `00000000                                     ;; 08:5a0c $00 $00
+    dw   `00000000                                     ;; 08:5a0e $00 $00
+
+    db   $00, $00, $00, $00, $00, $00, $00, $00        ;; 08:5a10 ????????
+    db   $00, $00, $00, $00, $00, $00, $00, $00        ;; 08:5a18 ????????
+    db   $00, $00, $00, $00, $00, $00, $00, $00        ;; 08:5a20 ????????
+    db   $00, $00, $00, $00, $00, $00, $00, $00        ;; 08:5a28 ????????
+    db   $00, $00, $00, $00, $00, $00, $00, $00        ;; 08:5a30 ????????
+    db   $00, $00, $00, $00, $00, $00, $00, $00        ;; 08:5a38 ????????
+
+;@gfximg name=player width=2 height=38
+data_08_5a40:
+    INCBIN "player.bin"                                ;; 08:5a40
 
 ;@gfximg name=chest width=2 height=3
 gfxChest:
@@ -48,19 +65,89 @@ gfxChest:
     INCBIN "statusbar.bin"                             ;; 08:6700
 ;@gfximg name=font width=16 height=6
     INCBIN "font.bin"                                  ;; 08:6a00
-;@gfximg name=attacks1 width=2 height=24
-    INCBIN "attacks1.bin"                              ;; 08:7000
-;@gfximg name=attacks2 width=20 height=1
-    INCBIN "attacks2.bin"                              ;; 08:7300
-;@gfximg name=attacks3 width=2 height=14
-    INCBIN "attacks3.bin"                              ;; 08:7440
+
+;@gfximg name=attack/sword width=2 height=6
+data_08_7000:
+    INCBIN "attack/sword.bin"                          ;; 08:7000
+
+;@gfximg name=attack/axe width=2 height=6
+data_08_70c0:
+    INCBIN "attack/axe.bin"                            ;; 08:70c0
+
+;@gfximg name=attack/morning_star width=2 height=6
+data_08_7180:
+    INCBIN "attack/morning_star.bin"                   ;; 08:7180
+
+;@gfximg name=attack/sickle width=2 height=6
+data_08_7240:
+    INCBIN "attack/sickle.bin"                         ;; 08:7240
+
+;@gfximg name=attacks1 width=6 height=1
+data_08_7300:
+    INCBIN "attacks1.bin"                              ;; 08:7300
+
+;@gfximg name=attacks2 width=14 height=1
+data_08_7360:
+    INCBIN "attacks2.bin"                              ;; 08:7360
+
+;@gfximg name=attack/mattok width=2 height=6
+data_08_7440:
+    INCBIN "attack/mattok.bin"                         ;; 08:7440
+
+;@gfximg name=attacks3 width=2 height=4
+data_08_7500:
+    INCBIN "attacks3.bin"                              ;; 08:7500
+
+data_08_7580:
+    db   $00, $00, $00, $00, $00, $00, $01, $01        ;; 08:7580 ????????
+    db   $06, $07, $05, $06, $0b, $0c, $0b, $0c        ;; 08:7588 ????????
+    db   $00, $00, $00, $00, $00, $00, $80, $80        ;; 08:7590 ????????
+    db   $60, $e0, $a0, $60, $d0, $30, $d0, $30        ;; 08:7598 ????????
+    db   $0b, $0c, $05, $06, $06, $07, $01, $01        ;; 08:75a0 ????????
+    db   $00, $00, $00, $00, $00, $00, $00, $00        ;; 08:75a8 ????????
+    db   $d0, $30, $a0, $60, $60, $e0, $80, $80        ;; 08:75b0 ????????
+    db   $00, $00, $00, $00, $00, $00, $00, $00        ;; 08:75b8 ????????
+    db   $00, $00, $03, $03, $0c, $0f, $11, $1e        ;; 08:75c0 ????????
+    db   $27, $38, $2f, $30, $4f, $70, $5f, $60        ;; 08:75c8 ????????
+    db   $00, $00, $c0, $c0, $30, $f0, $88, $78        ;; 08:75d0 ????????
+    db   $e4, $1c, $f4, $0c, $f2, $0e, $fa, $06        ;; 08:75d8 ????????
+    db   $5f, $60, $4f, $70, $2f, $30, $27, $38        ;; 08:75e0 ????????
+    db   $11, $1e, $0c, $0f, $03, $03, $00, $00        ;; 08:75e8 ????????
+    db   $fa, $06, $f2, $0e, $f4, $0c, $e4, $1c        ;; 08:75f0 ????????
+    db   $88, $78, $30, $f0, $c0, $c0, $00, $00        ;; 08:75f8 ????????
 
 ;@gfximg name=effect/snowman width=2 height=2
 gfxSnowman:
     INCBIN "effect/snowman.bin"                        ;; 08:7600
-;@gfximg name=attacks4 width=2 height=24
-    INCBIN "attacks4.bin"                              ;; 08:7640
+
+;@gfximg name=attack/ice width=2 height=10
+data_08_7640:
+    INCBIN "attack/ice.bin"                            ;; 08:7640
+
+;@gfximg name=attack/fire width=2 height=6
+data_08_7780:
+    INCBIN "attack/fire.bin"                           ;; 08:7780
+
+;@gfximg name=attacks4 width=2 height=8
+data_08_7840:
+    INCBIN "attacks4.bin"                              ;; 08:7840
+
 ;@gfximg name=attacks5 width=1 height=1
+data_08_7940:
     INCBIN "attacks5.bin"                              ;; 08:7940
-;@gfximg name=attacks6 width=2 height=39
+
+;@gfximg name=attacks6 width=2 height=2
+data_08_7950:
     INCBIN "attacks6.bin"                              ;; 08:7950
+
+;@gfximg name=attacks7 width=2 height=2
+data_08_7990:
+    INCBIN "attacks7.bin"                              ;; 08:7990
+
+;@gfximg name=attacks8 width=2 height=31
+data_08_79d0:
+    INCBIN "attacks8.bin"                              ;; 08:79d0
+
+;@gfximg name=attacks9 width=2 height=5
+data_08_7db0:
+    INCBIN "attacks9.bin"                              ;; 08:7db0
