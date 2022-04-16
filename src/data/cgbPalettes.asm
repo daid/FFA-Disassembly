8a5275c97c8585d8009451ff0b90247bf6c29132
Daid <daid303@gmail.com>
2022-04-16 16:44:26 +0200

    Prepare for ingame palette switching on maps.

--- src/data/cgbPalettes.asm
+++ src/data/cgbPalettes.asm
@@ -1,12 +1,12 @@
 cgbPaletteTable:
     ; $00 Overworld background palette
-    dw $47FF, $51F3, $2867, $0000
-    dw $47FF, $26C4, $1521, $0000
-    dw $47FF, $11D9, $10CE, $0000
-    dw $47FF, $1419, $100A, $0000
-    dw $47FF, $12EC, $15A6, $0000
-    dw $47FF, $7CC3, $2C00, $0000
-    dw $0000, $0000, $0000, $0000
+    dw $47FF, $51F3, $2867, $0000 ; grey/ui
+    dw $47FF, $26C4, $1521, $0000 ; dark green
+    dw $47FF, $11D9, $10CE, $0000 ; brown
+    dw $47FF, $1419, $100A, $0000 ; red
+    dw $47FF, $12EC, $15A6, $0000 ; light green
+    dw $47FF, $7CC3, $2C00, $0000 ; dark blue (water)
+    dw $12EC, $11D9, $10CE, $0ce2 ; brown/green combo
     dw $0000, $0000, $0000, $0000
 
     ; $01 Main object palette
@@ -24,7 +24,17 @@ cgbPaletteTable:
     dw $47FF, $1aec, $1a4a, $0000 ; light & dark green colors  (for main letters)
     dw $47FF, $0000, $0000, $0000
     dw $47FF, $4a73, $31ad, $0000 ; sword greys
-    dw $47FF, $26f8, $51cc, $0000 ; sword yellow and blue
-    dw $47FF, $1cf1, $1089, $0000 ; sword reds
+    dw $47FF, $16f8, $51cc, $0000 ; sword yellow and blue
+    dw $47FF, $1cf7, $108d, $0000 ; sword reds
     dw $47FF, $0000, $0000, $0000
+    dw $0000, $0000, $0000, $0000
+
+    ; $03 Cave background palette
+    dw $47FF, $51F3, $2867, $0000 ; greys
+    dw $47FF, $26C4, $1521, $0000 ; green / plants
+    dw $47FF, $11D9, $10CE, $0000 ; browns
+    dw $47FF, $1419, $100A, $0000 ; red/lava
+    dw $47FF, $7dc9, $7CC3, $0000 ; ice
+    dw $47FF, $7CC3, $2C00, $0000 ; dark blue water
+    dw $0000, $0000, $0000, $0000
     dw $0000, $0000, $0000, $0000
