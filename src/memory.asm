686ca49d78f9878d39a2366cb32e245aa426c666
Daid <daid303@gmail.com>
2023-01-08 11:08:41 +0000

    update

--- src/memory.asm
+++ src/memory.asm
@@ -237,13 +237,11 @@ wMusicVibratoDurationChannel3:
 
 wMusicVibratoEnvelopeChannel3:
     ds 1                                               ;; c137
-
 .high:
     ds 1                                               ;; c138
 
 wMusicVibratoEnvelopePointerChannel3:
     ds 1                                               ;; c139
-
 .high:
     ds 1                                               ;; c13a
 
